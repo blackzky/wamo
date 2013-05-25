4092ea0c21cfe43bed64bcf28dc34f5a99ec12d5
Gervel Giva <gervel@caresharing.ph>
2013-05-25 12:34:19 +0800

    refactored some code --added support for portal commands

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,34 +1,85 @@
-$(document).ready(function() {
-  var CLIENT_KEYS = {
-    "submit" : 13,
-    "focus-command" : 27
+var PLACE = "portal";
+var CLIENT_CMD = {
+  SUBMIT : 13,
+  FOCUS : 27
+};
+var CMD = {
+  "portal" :{
+    "login": function(){
+      $("#block").load("login", refocus);
+    },
+    "register": function(){
+      $("#block").load("register", refocus);
+    },
+    "help":function(){
+      alert("help");
+    },
+    "exit": function(){
+      alert("exit");
+    }
   }
-  refocus();
-  $(document).keyup(function(e){
-    if(e.keyCode == CLIENT_KEYS["focus-command"]){
-      console.log("esc");
-      $("#command-line").focus();
-    }else{
-      console.log(e.keyCode);
-    }
-  });
-  $("#command-line").keypress(function(e){
-    if(e.which == CLIENT_KEYS["submit"]){
-      if(this.value == "login"){
-        console.log("login");
-        $("#block").load("login", refocus);
-      }else if(this.value == "register"){
-        $("#block").load("register");
-      }else{
-        console.log(this.value);
+};
 
+var FORM = {
+  "login": function(){
+    $.ajax({
+      type: "POST",
+      url: "login",
+      data: $("form#login").serialize(),
+      success: function(data, textStatus, xhr) {
+        if(data.substring(0, 20).indexOf("DOCTYPE") == -1){
+          $("#block").html(data);
+          refocus();
+        }else{
+          window.location = "/";
+        }
       }
-      this.value = "";
-    }
+    });
 
+    return false;
+  },
+  "register": function(){
+    $.ajax({
+      type: "POST",
+      url: "register",
+      data: $("form#register").serialize(),
+      success: function(data) {
+        $("#block").html(data);
+      }
+    });
+
+    refocus();
+    return false;
+  }
+};
+
+function refocus(){ $("input").first().focus(); }
+
+function execCmd(){
+  var command = $("#command-line").val().toLowerCase();
+  if(typeof(CMD[PLACE.toLowerCase()][command]) === 'undefined'){
+    $("#cmd-response p").text("Invalid Command");
+    $("#cmd-response").show();
+  }else{
+    CMD[PLACE.toLowerCase()][command]();
+    $("#cmd-response p").text("");
+    $("#cmd-response").hide();
+  }
+  $("#command-line").val("");
+}
+
+$(document).ready(function() {
+  refocus();
+  $("#cmd-response").hide();
+
+  $(document).keyup(function(e){
+    if(e.keyCode == CLIENT_CMD.FOCUS){
+      $("#command-line").focus();
+    }else if(e.keyCode == CLIENT_CMD.SUBMIT){
+      if(e.target.id != "command-line"){
+        FORM[$(e.target).parents().find("form").attr("id").toLowerCase()]();
+      }else { execCmd(); }
+    }
   });
 });
 
-function refocus(){
-  $("input").first().focus();
-}
